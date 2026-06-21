5292cea74ecfb3286bc323e38bb7d8a3e9719a4b
Madhur Ahuja <ahujamadhur2016@gmail.com>
2026-06-21 13:27:56 +0530

    Add PostgreSQL and environment variable configuration

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-connect_args = {"check_same_thread": False}
+load_dotenv()
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(bind=engine,autocommit=False, autoflush=False)
